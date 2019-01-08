79d720f8893e2dbd8f242130e3f112ae4ea1fdf6
yoga <yogamadness@gmail.com>
2019-01-08 16:26:53 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+final ThemeData KIOSTheme = new ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.blue,
+  accentColor: Colors.orangeAccent[100],
+);
+
+/* defaultTargetPlatform == TargetPlatform.iOS
+          ? KIOSTheme
+          : kDefaultTheme */
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.brown,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPage createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        toolbarOpacity: 0,
+      ),
+      body: Builder(
+          builder: (context) => Container(
+                decoration: new BoxDecoration(
+                  image: new DecorationImage(
+                  image: new AssetImage("images/lake.jpg"),
+                  fit: BoxFit.cover,
+                )),
+                child: Center(
+                    child: Container(
+                        width: 400,
+                        height: 400,
+                        margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              decoration: InputDecoration(
+                                  labelText: ('Enter Your Email'),
+                                  suffixIcon:
+                                      Icon(Icons.account_circle, size: 30)),
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: ('Enter Your Password'),
+                                suffixIcon: Icon(
+                                  Icons.lock,
+                                  size: 30,
+                                ),
+                              ),
+                            ),
+                            new Container(
+                              width: 400,
+                              child: RaisedButton(
+                                  textColor: Colors.white,
+                                  color: Colors.blueAccent,
+                                  child: Text('Login'),
+                                  onPressed: () => _submitPressed(context)),
+                            )
+                          ],
+                        ))),
+              )),
+    );
+  }
+
+  void _submitPressed(BuildContext context) {
+    /*Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text('This is snackbar .'),
+      action: SnackBarAction(
+          label: 'ACTION',
+          onPressed: () {
+            Scaffold.of(context)
+                .showSnackBar(SnackBar(content: Text('You pressed snackbar')));
+          }),
+    )); */
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
